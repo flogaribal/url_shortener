157c7bd62ce305b8159482c76de4d6ec15d26b70
Guillaume Hottin <guillaume.louis.hottin@gmail.com>
2018-11-15 14:20:20 +0100

    Added pymongo requirement

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@
 
 from distutils.core import setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(name='UrlShortener',
       version='1.0',
       description='Url Shortener Library',
       author='Guillaume Hottin',
       packages=['url_shortener'],
+      install_requires=requirements,
      )
