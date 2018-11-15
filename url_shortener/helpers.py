c3fd66b94e562e53fae4f782ff267fbf38e6573a
Guillaume Hottin <guillaume.louis.hottin@gmail.com>
2018-11-15 16:28:29 +0100

    uir et authent

--- url_shortener/helpers.py
+++ url_shortener/helpers.py
@@ -61,10 +61,9 @@ def generate_short_url(mongo_host, mongo_port, mongo_user, mongo_password, db_na
 
 # MONGO CONNECTIONS
 def connect_to_mongo(mongo_host, mongo_port, mongo_user, mongo_password):
-    uri = "mongodb+srv://%s:%s@%s" % (
-        quote_plus(mongo_user), quote_plus(mongo_password), mongo_host)
+    uri = f"mongodb+srv:/{mongo_host}"
     print(uri)
-    return MongoClient(uri)
+    return MongoClient(uri, username=mongo_user, password=mongo_password)
 
 
 def get_db(client, db_name):
